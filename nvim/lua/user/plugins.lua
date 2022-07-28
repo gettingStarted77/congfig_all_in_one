2d88d44d80290533de4e7d283714a6688b9bfccb
im <ispingzhou@gmail.com>
2022-07-28 16:31:21 +0800

    add nvim-treesitter for better highlighting

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -83,8 +83,8 @@ return packer.startup(function(use)
     "nvim-lualine/lualine.nvim",
     requires = { "kyazdani42/nvim-web-devicons", opt = true }
   }
-
-
+  -- Tree-sitter 
+  use {"nvim-treesitter/nvim-treesitter",run = ":TSUpdate"}
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
