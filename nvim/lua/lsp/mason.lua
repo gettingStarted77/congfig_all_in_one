daf4b030111da1ca8e32f106e828e7ee1969ae4f
im <lpz0401@163.com>
2022-10-10 23:24:20 +0800

    Maybe I initially completed the config of mason and mason-lspconfig

--- nvim/lua/lsp/mason.lua
+++ nvim/lua/lsp/mason.lua
@@ -1,29 +1,10 @@
-local status_ok, mason = pcall(require, "mason")
-if not status_ok then
-  return
-end
-
-local status_ok_1, mason_lspconfig = pcall(require, "mason-lspconfig")
-if not status_ok_1 then
-  return
-end
-
-local servers = {
-  "lua-language-server",
-  "rust_analyzer",
-  "codelldb",
-}
-
-local settings = {
-  ui = {
-    border = "rounded",
-  },
-  log_level = vim.log.levels.INFO,
-  max_concurrent_installers = 4,
-}
-
-mason.setup(settings)
-mason_lspconfig.setup {
-  ensure_installed = servers,
-  automatic_installation = true,
-}
+require("mason").setup({
+    ui = {
+        border = "rounded",
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
+})
