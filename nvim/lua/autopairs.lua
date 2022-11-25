b361ae94486ee3e9fb975d6e66bf64a040b42722
im <lpz0401@163.com>
2022-11-25 21:23:07 +0800

    add rule for

--- nvim/lua/autopairs.lua
+++ nvim/lua/autopairs.lua
@@ -1,1 +1,7 @@
-require("nvim-autopairs").setup {}
+local npairs = require('nvim-autopairs')
+npairs.setup{}
+
+-- add pair <> in *.rs
+local Rule = require('nvim-autopairs.rule')
+npairs.remove_rule('<')
+npairs.add_rule(Rule("<", ">"))
